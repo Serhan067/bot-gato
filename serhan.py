82785ffa7b0a3bbc5b041c67a42f3648c4e2df2e
Serhan067 <basserhan23@gmail.com>
2025-01-24 19:15:21 +0300

    Update serhan.py

--- serhan.py
+++ serhan.py
@@ -1,1 +1,49 @@
+import speech_recognition
+
+def kontr():
+    mikrofon=speech_recognition.Microphone()
+    kayit=speech_recognition.Recognizer()
+
+    with mikrofon as ses:
+        kayit.adjust_for_ambient_noise(ses)
+
+        serhan=kayit.listen(ses)
+        try:
+
+            return kayit.recognize_google(serhan, language="tr-TR")
+        except:
+            return "Anlamadım, Tekrarla"
+
+def konen():
+    mikrofon=speech_recognition.Microphone()
+    kayit=speech_recognition.Recognizer()
+
+    with mikrofon as ses:
+        kayit.adjust_for_ambient_noise(ses)
+
+        serhan=kayit.listen(ses)
+        try:
+
+            return kayit.recognize_google(serhan, language="en-US")
+        except:
+            return "i dont get it, please repeat it"
+
+
+
+if __name__ == "__main__":
+
+    print("Sesiniz şimdi kaydediliyor")
+
+    gosling=kontr()
+
+    print(gosling)
+
+
+
+
+
+
+
+
+
 
